d01e5c8b9807a96bdebef7f67c48f57fae029120
developeridealancy <developer.idealancy@gmail.com>
2020-09-29 01:31:49 +0500

    aciton file updated

--- src/lib/redux.ts
+++ src/lib/redux.ts
@@ -6,9 +6,10 @@ import { TaskType } from '../components/Task';
 const defaultTasks: { tasks: TaskType[] } = {
     tasks: [
         { id: '1', title: 'Something', state: 'TASK_INBOX', updatedAt: new Date() },
-        { id: '2', title: 'Something more', state: 'TASK_INBOX', updatedAt: new Date() },
-        { id: '3', title: 'Something else', state: 'TASK_INBOX', updatedAt: new Date() },
-        { id: '4', title: 'Something again', state: 'TASK_INBOX', updatedAt: new Date() },
+        { id: '2', title: 'Something new', state: 'TASK_INBOX', updatedAt: new Date() },
+        { id: '3', title: 'Something more', state: 'TASK_INBOX', updatedAt: new Date() },
+        { id: '4', title: 'Something else', state: 'TASK_INBOX', updatedAt: new Date() },
+        { id: '5', title: 'Something again', state: 'TASK_INBOX', updatedAt: new Date() },
     ]
 }
 
